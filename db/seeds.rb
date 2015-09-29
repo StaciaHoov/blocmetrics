37668295012e27aa126a5deec471d84dc01a0429
staciahoov <staciaray@roadrunner.com>
2015-09-29 19:10:14 +0000

    added events and grouped by name on app show

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,51 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'faker'
+
+
+5.times do
+    user = User.create!(
+        name: Faker::Name.name,
+        email: Faker::Internet.email,
+        password: Faker::Lorem.characters(10)
+        )
+    user.skip_confirmation!
+    user.save!
+end
+users = User.all
+
+10.times do
+    RegisteredApplication.create!(
+        user: users.sample,
+        url: Faker::Internet.url,
+        name: Faker::App.name
+    )
+end
+registered_applications = RegisteredApplication.all
+
+100.times do
+    Event.create!(
+        registered_application: registered_applications.sample,
+        name: Faker::Lorem.word
+    )
+end
+events = Event.all
+
+
+stacia = User.new(
+    name: 'stacia',
+    password: 'helloworld',
+    email: 'stacia@example.com'
+    )
+stacia.skip_confirmation!
+stacia.save!
+
+admin = User.new(
+    name: 'admin',
+    password: 'helloworld',
+    email: 'admin@example.com'
+    )
+admin.skip_confirmation!
+admin.save!
+
+puts "#{User.count} users created."
+puts "#{RegisteredApplication.count} registered_applications created."
+puts "#{Event.count} events created."
