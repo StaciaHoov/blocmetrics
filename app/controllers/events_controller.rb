b7042b8e604ebbb1faf592d756a1168c6c438499
staciahoov <staciaray@roadrunner.com>
2015-10-01 19:47:40 +0000

    progress on api controller

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,34 @@
+class API::EventsController < ApplicationController
+    skip_before_action :verify_authenticity_token
+    
+    before_filter :set_access_control_headers
+    
+    def set_access_control_headers
+        headers['Access-Control-Allow-Origin'] = '*' # allow requests from any origin.
+        headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS' #permit the POST, GET, OPTIONS request methods.
+        headers['Access-Control-Allow-Headers'] = 'Content-Type' # allow the header Content-Type, - used in HTTP requests to declare the type of the data being sent.
+    end
+    
+    def create
+        @registered_application = RegisteredApplication.find_by(url: request.env['HTTP_ORIGIN'])
+        
+        if @registered_application == nil
+           render json: "Unregistered application", status: :unprocessable_entity
+       else
+            @event = @registered_application.events.build(event_params)
+        
+            if @event.save
+                render json: @event, status: :created
+            else
+                render @event.errors, status: :unprocessable_entity
+            end
+        end
+    end
+    
+    
+    private
+    
+    def event_params
+        params.permit(:name)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,12 @@ Rails.application.routes.draw do
   get 'welcome/about'
   
   resources :registered_applications
+  
+  namespace :api, defaults: { format: :json } do
+    match '/events', to: 'events#index', via: [:options]
+    resources :events, only: [:create]
+  end
+  
+  
 
 end
